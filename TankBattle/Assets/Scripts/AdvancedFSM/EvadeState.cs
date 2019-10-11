dde3bd159f9175f8b34d8c738e402977c71e57bf
Thom Martens <thommartens14@hotmail.com>
2019-10-11 12:39:50 +0200

    Initial commit

---
+++ TankBattle/Assets/Scripts/AdvancedFSM/EvadeState.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvadeState : FSMState
+{
+    private float timePassed;
+    private Transform otherTank;
+    public EvadeState()
+    {
+        stateID = FSMStateID.Evading;
+        curSpeed = 125f;
+        curRotSpeed = 1.0f;
+    }
+
+    public override void Reason(Transform player, Transform npc)
+    {
+        if (timePassed > 0.5f)
+        {
+            timePassed = 0f;
+            npc.GetComponent<NPCTankController>().SetTransition(Transition.LostPlayer);
+        }
+    }
+
+    public override void Act(Transform player, Transform npc)
+    {
+    }
+
+    public override void ActWithoutPlayer(Transform lhs, Transform rhs)
+    {
+        if (otherTank == null)
+        {
+            otherTank = lhs;
+        }
+        else if (!otherTank.Equals(lhs))
+        {
+            otherTank = lhs;
+        }
+
+        Vector3 obstaclePos = otherTank.position;
+
+        Quaternion targetRotation = Quaternion.LookRotation(rhs.transform.position - obstaclePos);
+        rhs.rotation = Quaternion.Slerp(rhs.rotation, targetRotation, Time.deltaTime * curRotSpeed);
+
+        rhs.Translate(Vector3.forward * Time.deltaTime * curSpeed);
+
+        timePassed += Time.deltaTime;
+    }
+}
---
+++ TankBattle/Assets/Scripts/AdvancedFSM/FleeState.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeState : FSMState
+{
+
+    private NPCTankController npcController;
+
+    public FleeState()
+    {
+        stateID = FSMStateID.Fleeing;
+        curSpeed = 125f;
+        curRotSpeed = 1.0f;
+    }
+    public override void Reason(Transform player, Transform npc)
+    {
+        npcController = npc.GetComponent<NPCTankController>();
+
+        //Set the target position as the player position
+        destPos = player.position;
+
+        //Check the distance with player tank
+        //When the distance is near, transition to attack state
+        float dist = Vector3.Distance(npc.position, destPos);
+
+        if (npcController.GetCurrentHealth() >= (npcController.GetMaxHealth() / 1.5) && dist < 200f)
+        {
+            Debug.Log(npcController.GetCurrentHealth() + "Current health " + npcController.GetMaxHealth() + " Max health");
+            Debug.Log("Switch to attack state");
+            npcController.SetTransition(Transition.ReachPlayer);
+        } else if (dist > 800f)
+        {
+            npcController.SetTransition(Transition.LostPlayer);
+        }
+    }
+
+    public override void Act(Transform player, Transform npc)
+    {
+        destPos = player.position;
+
+        Quaternion targetRotation = Quaternion.LookRotation(npc.position - destPos);
+        npc.rotation = Quaternion.Slerp(npc.rotation, targetRotation, Time.deltaTime * curRotSpeed);
+
+        npc.Translate(Vector3.forward * Time.deltaTime * curSpeed);
+    }
+}
